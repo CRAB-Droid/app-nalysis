8e83a42d04ffd33b77abeb754da2fb2b0cf7adf4
Matthew Berthoud <mwberthoud@wm.edu>
2024-05-10 21:31:51 -0400

    attempt at using API how it says to be used

--- scripts/testscript.py
+++ scripts/testscript.py
@@ -18,32 +18,55 @@ _a, _vm, _vmx = AnalyzeAPK("../apks/GCash.apk")
 
 print("\n\n------- A obj permission APIs -------\n")
 
-a_perms = _a.get_permissions()
-print("\na: " + str(a_perms))
-
-a_dec_perms = _a.get_declared_permissions()
-print("\na declared permissions: " + str(a_perms))
-
-a_dec_perms_details = _a.get_declared_permissions_details()
-print("\na declared permissions details: " + str(a_dec_perms_details))
-
-a_get_details_permissions = _a.get_details_permissions()
-print("\na get_details_permissions: " + str(a_get_details_permissions ))
-
-a_get_requested_aosp_permissions = _a.get_requested_aosp_permissions()
-print("\na get_requested_aosp_permissions: " + str(a_get_requested_aosp_permissions))
-
-a_get_requested_aosp_permissions_details = _a.get_requested_aosp_permissions_details()
-print("\na get_requested_aosp_permissions_details: " + str(a_get_requested_aosp_permissions_details))
-
-a_get_requested_third_party_permissions = _a.get_requested_third_party_permissions()
-print("\na get_requested_third_party_permissions:" + str(a_get_requested_third_party_permissions))
-
-a_get_uses_implied_permission_list = _a.get_uses_implied_permission_list()
-print("\na get_uses_implied_permission_list: " + str(a_get_uses_implied_permission_list))
+perms = _a.get_permissions()
+print("\na: " + str(perms))
 
 
-print("\n\n------- VM obj permission APIs -------\n")
+for meth, perm in _vmx.get_permissions(_a.get_effective_target_sdk_version()):
+    if meth.is_external():
+        continue
+    print(f"Using API method {meth} for permission {perm} used in:")
+    for _, m, _ in meth.get_xref_from():
+        print(m.full_name)
+
+
+# for _class in _vmx.get_classes():
+#     for _method in _class.get_methods():
+#         if (_method.is_external()):
+#             continue
+
+        
+
+
+
+
+
+
+
+# a_dec_perms = _a.get_declared_permissions()
+# print("\na declared permissions: " + str(a_perms))
+
+# a_dec_perms_details = _a.get_declared_permissions_details()
+# print("\na declared permissions details: " + str(a_dec_perms_details))
+
+# a_get_details_permissions = _a.get_details_permissions()
+# print("\na get_details_permissions: " + str(a_get_details_permissions ))
+
+# a_get_requested_aosp_permissions = _a.get_requested_aosp_permissions()
+# print("\na get_requested_aosp_permissions: " + str(a_get_requested_aosp_permissions))
+
+# a_get_requested_aosp_permissions_details = _a.get_requested_aosp_permissions_details()
+# print("\na get_requested_aosp_permissions_details: " + str(a_get_requested_aosp_permissions_details))
+
+# a_get_requested_third_party_permissions = _a.get_requested_third_party_permissions()
+# print("\na get_requested_third_party_permissions:" + str(a_get_requested_third_party_permissions))
+
+# a_get_uses_implied_permission_list = _a.get_uses_implied_permission_list()
+# print("\na get_uses_implied_permission_list: " + str(a_get_uses_implied_permission_list))
+
+
+
+# print("\n\n------- VM obj permission APIs -------\n")
 
 
 # dex_num = 0
